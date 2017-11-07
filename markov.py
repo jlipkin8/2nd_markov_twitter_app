9e2f241986cf0b7813ca02a1a5d3faf446d9875d
Josie Lipkin <jlipkin8@gmail.com>
2017-11-06 18:01:09 -0800

    created a requirements file

--- markov.py
+++ markov.py
@@ -1,4 +1,8 @@
 # import necessary libraries 
+import os # to access our OS environment variables 
+
+import twitter 
+# pip install python twitter into an active virtaul env
 
 # read tweets and create corpus 
 
