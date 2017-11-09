d38fbc29d841c06885cf6ee146ec0a68a71de8a8
jlipkin8 <jlipkin8@gmail.com>
2017-11-09 21:48:31 +0000

    saved statuses from certain handles in variables, contained all the tweets in onevariable

--- markov.py
+++ markov.py
@@ -15,16 +15,31 @@ api = twitter.Api(
 # This will print info about credentials to make sure they're correct 
 # print api.VerifyCredentials()
 
-followers = api.GetFollowers()
+# followers = api.GetFollowers()
 
-for follower in followers: 
-    print "Name: ", follower.name
-    print "Number of Friends: ", follower.friends_count
-    if follower.geo_enabled: 
-        print follower.location
+# for follower in followers: 
+#     print "Name: ", follower.name
+#     print "Number of Friends: ", follower.friends_count
+#     if follower.geo_enabled: 
+#         print follower.location
+
+
+statuses_1 = api.GetUserTimeline(None,"WebMD")
+statuses_2 = api.GetUserTimeline(None, "epicurious")
 
 # read tweets and create corpus 
+def create_corpus(text, text_2): 
+    corpus = ""
+    for s in text: 
+        corpus += s.text 
 
+    for s in text_2: 
+        corpus += s.text 
+
+    return corpus 
+
+
+print create_corpus(statuses_1, statuses_2)
 # make_chains 
 
 # make text
